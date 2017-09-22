ee9bbfd25ad79e8899ae9d6d1f9dcf256965e6e3
Caleb <inchristalway-tech4him@yahoo.com>
2017-09-21 20:28:50 -0600

    Add tests from `node-slug`.

---
+++ __test__/slug.spec.js
@@ -0,0 +1,159 @@
+/** Copied from https://github.com/dodo/node-slug.
+Copyright (c) 2014 ▟ ▖▟ ▖(dodo)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**/
+import urlize from '../src/urlize';
+
+function charMapHelper(char_map) {
+  for (const char in char_map) {
+    const replacement = char_map[char].toLowerCase();
+    const expected = (replacement ? `foo-${replacement}-bar-baz` : "foo-bar-baz");
+    expect(urlize(`foo ${char} bar baz`)).toEqual(expected);
+  }
+}
+
+describe('urlize', () => {
+  it('should replace whitespaces with replacement', () => {
+    expect(urlize('foo bar baz')).toEqual('foo-bar-baz');
+  });
+  it('should remove trailing space if any', () => {
+    expect(urlize(' foo bar baz ')).toEqual('foo-bar-baz');
+  });
+  it('should remove not allowed chars', () => {
+    expect(urlize('foo, bar baz')).toEqual('foo-bar-baz');
+    expect(urlize('foo- bar baz')).toEqual('foo-bar-baz');
+    expect(urlize('foo] bar baz')).toEqual('foo-bar-baz');
+  });
+  it('should leave allowed chars in rfc3986 mode', () => {
+    const allowed = ['.', '_', '~'];
+    for (const a of allowed) {
+      expect(
+        urlize(`foo ${a} bar baz`,
+        {
+          mode: "rfc3986"
+        })
+      ).toEqual(`foo-${a}-bar-baz`);
+    }
+  });
+  it('should leave allowed chars in pretty mode', () => {
+    const allowed = ['_', '~'];
+    for (const a of allowed) {
+      expect(urlize(`foo ${a} bar baz`)).toEqual(`foo-${a}-bar-baz`);
+    }
+  });
+  it('should replace latin chars', () => {
+    charMapHelper({
+      'À': 'A', 'Á': 'A', 'Â': 'A', 'Ã': 'A', 'Ä': 'A', 'Å': 'A', 'Æ': 'AE',
+      'Ç': 'C', 'È': 'E', 'É': 'E', 'Ê': 'E', 'Ë': 'E', 'Ì': 'I', 'Í': 'I',
+      'Î': 'I', 'Ï': 'I', 'Ð': 'D', 'Ñ': 'N', 'Ò': 'O', 'Ó': 'O', 'Ô': 'O',
+      'Õ': 'O', 'Ö': 'O', 'Ő': 'O', 'Ø': 'O', 'Ù': 'U', 'Ú': 'U', 'Û': 'U',
+      'Ü': 'U', 'Ű': 'U', 'Ý': 'Y', 'Þ': 'TH', 'ß': 'ss', 'à':'a', 'á':'a',
+      'â': 'a', 'ã': 'a', 'ä': 'a', 'å': 'a', 'æ': 'ae', 'ç': 'c', 'è': 'e',
+      'é': 'e', 'ê': 'e', 'ë': 'e', 'ì': 'i', 'í': 'i', 'î': 'i', 'ï': 'i',
+      'ð': 'd', 'ñ': 'n', 'ò': 'o', 'ó': 'o', 'ô': 'o', 'õ': 'o', 'ö': 'o',
+      'ő': 'o', 'ø': 'o', 'ù': 'u', 'ú': 'u', 'û': 'u', 'ü': 'u', 'ű': 'u',
+      'ý': 'y', 'þ': 'th', 'ÿ': 'y', 'ẞ': 'SS',
+    });
+  });
+  it('should replace greek chars', () => {
+    charMapHelper({
+      'α':'a', 'β':'b', 'γ':'g', 'δ':'d', 'ε':'e', 'ζ':'z', 'η':'h', 'θ':'8',
+      'ι':'i', 'κ':'k', 'λ':'l', 'μ':'m', 'ν':'n', 'ξ':'3', 'ο':'o', 'π':'p',
+      'ρ':'r', 'σ':'s', 'τ':'t', 'υ':'y', 'φ':'f', 'χ':'x', 'ψ':'ps', 'ω':'w',
+      'ά':'a', 'έ':'e', 'ί':'i', 'ό':'o', 'ύ':'y', 'ή':'h', 'ώ':'w', 'ς':'s',
+      'ϊ':'i', 'ΰ':'y', 'ϋ':'y', 'ΐ':'i',
+      'Α':'A', 'Β':'B', 'Γ':'G', 'Δ':'D', 'Ε':'E', 'Ζ':'Z', 'Η':'H', 'Θ':'8',
+      'Ι':'I', 'Κ':'K', 'Λ':'L', 'Μ':'M', 'Ν':'N', 'Ξ':'3', 'Ο':'O', 'Π':'P',
+      'Ρ':'R', 'Σ':'S', 'Τ':'T', 'Υ':'Y', 'Φ':'F', 'Χ':'X', 'Ψ':'PS', 'Ω':'W',
+      'Ά':'A', 'Έ':'E', 'Ί':'I', 'Ό':'O', 'Ύ':'Y', 'Ή':'H', 'Ώ':'W', 'Ϊ':'I',
+      'Ϋ':'Y',
+    });
+  });
+  it('should replace turkish chars', () => {
+    charMapHelper({
+      'ş':'s', 'Ş':'S', 'ı':'i', 'İ':'I', 'ç':'c', 'Ç':'C', 'ü':'u', 'Ü':'U',
+      'ö':'o', 'Ö':'O', 'ğ':'g', 'Ğ':'G',
+    });
+  });
+  it('should replace cyrillic chars', () => {
+    charMapHelper({
+      'а':'a', 'б':'b', 'в':'v', 'г':'g', 'д':'d', 'е':'e', 'ё':'yo', 'ж':'zh',
+      'з':'z', 'и':'i', 'й':'j', 'к':'k', 'л':'l', 'м':'m', 'н':'n', 'о':'o',
+      'п':'p', 'р':'r', 'с':'s', 'т':'t', 'у':'u', 'ф':'f', 'х':'h', 'ц':'c',
+      'ч':'ch', 'ш':'sh', 'щ':'sh', 'ъ':'u', 'ы':'y', 'ь':'', 'э':'e', 'ю':'yu',
+      'я':'ya',
+      'А':'A', 'Б':'B', 'В':'V', 'Г':'G', 'Д':'D', 'Е':'E', 'Ё':'Yo', 'Ж':'Zh',
+      'З':'Z', 'И':'I', 'Й':'J', 'К':'K', 'Л':'L', 'М':'M', 'Н':'N', 'О':'O',
+      'П':'P', 'Р':'R', 'С':'S', 'Т':'T', 'У':'U', 'Ф':'F', 'Х':'H', 'Ц':'C',
+      'Ч':'Ch', 'Ш':'Sh', 'Щ':'Sh', 'Ъ':'U', 'Ы':'Y', 'Ь':'', 'Э':'E', 'Ю':'Yu',
+      'Я':'Ya', 'Є':'Ye', 'І':'I', 'Ї':'Yi', 'Ґ':'G', 'є':'ye', 'і':'i', 'ї':'yi', 'ґ':'g',
+    });
+  });
+  it('should replace czech chars', () => {
+    charMapHelper({
+      'č':'c', 'ď':'d', 'ě':'e', 'ň': 'n', 'ř':'r', 'š':'s', 'ť':'t', 'ů':'u',
+      'ž':'z', 'Č':'C', 'Ď':'D', 'Ě':'E', 'Ň': 'N', 'Ř':'R', 'Š':'S', 'Ť':'T',
+      'Ů':'U', 'Ž':'Z',
+    });
+  });
+  it('should replace polish chars', () => {
+    charMapHelper({
+      'ą':'a', 'ć':'c', 'ę':'e', 'ł':'l', 'ń':'n', 'ó':'o', 'ś':'s', 'ź':'z',
+      'ż':'z', 'Ą':'A', 'Ć':'C', 'Ę':'E', 'Ł':'L', 'Ń':'N', 'Ś':'S',
+      'Ź':'Z', 'Ż':'Z',
+    });
+  });
+  it('should replace latvian chars', () => {
+    charMapHelper({
+      'ā':'a', 'č':'c', 'ē':'e', 'ģ':'g', 'ī':'i', 'ķ':'k', 'ļ':'l', 'ņ':'n',
+      'š':'s', 'ū':'u', 'ž':'z', 'Ā':'A', 'Č':'C', 'Ē':'E', 'Ģ':'G', 'Ī':'I',
+      'Ķ':'K', 'Ļ':'L', 'Ņ':'N', 'Š':'S', 'Ū':'U', 'Ž':'Z',
+    });
+  });
+  it('should replace vietnamese chars', () => {
+    charMapHelper({
+      'Ạ': 'A', 'Ả': 'A', 'Ầ': 'A', 'Ấ': 'A', 'Ậ': 'A', 'Ẩ': 'A', 'Ẫ': 'A',
+      'Ằ': 'A', 'Ắ': 'A', 'Ặ': 'A', 'Ẳ': 'A', 'Ẵ': 'A', 'Ẹ': 'E', 'Ẻ': 'E',
+      'Ẽ': 'E', 'Ề': 'E', 'Ế': 'E', 'Ệ': 'E', 'Ể': 'E', 'Ễ': 'E', 'Ị': 'I',
+      'Ỉ': 'I', 'Ĩ': 'I', 'Ọ': 'O', 'Ỏ': 'O', 'Ồ': 'O', 'Ố': 'O', 'Ộ': 'O',
+      'Ổ': 'O', 'Ỗ': 'O', 'Ơ': 'O', 'Ờ': 'O', 'Ớ': 'O', 'Ợ': 'O', 'Ở': 'O',
+      'Ỡ': 'O', 'Ụ': 'U', 'Ủ': 'U', 'Ũ': 'U', 'Ư': 'U', 'Ừ': 'U', 'Ứ': 'U',
+      'Ự': 'U', 'Ử': 'U', 'Ữ': 'U', 'Ỳ': 'Y', 'Ỵ': 'Y', 'Ỷ': 'Y', 'Ỹ': 'Y',
+      'Đ': 'D', 'ạ': 'a', 'ả': 'a', 'ầ': 'a', 'ấ': 'a', 'ậ': 'a', 'ẩ': 'a',
+      'ẫ': 'a', 'ằ': 'a', 'ắ': 'a', 'ặ': 'a', 'ẳ': 'a', 'ẵ': 'a', 'ẹ': 'e',
+      'ẻ': 'e', 'ẽ': 'e', 'ề': 'e', 'ế': 'e', 'ệ': 'e', 'ể': 'e', 'ễ': 'e',
+      'ị': 'i', 'ỉ': 'i', 'ĩ': 'i', 'ọ': 'o', 'ỏ': 'o', 'ồ': 'o', 'ố': 'o',
+      'ộ': 'o', 'ổ': 'o', 'ỗ': 'o', 'ơ': 'o', 'ờ': 'o', 'ớ': 'o', 'ợ': 'o',
+      'ở': 'o', 'ỡ': 'o', 'ụ': 'u', 'ủ': 'u', 'ũ': 'u', 'ư': 'u', 'ừ': 'u',
+      'ứ': 'u', 'ự': 'u', 'ử': 'u', 'ữ': 'u', 'ỳ': 'y', 'ỵ': 'y', 'ỷ': 'y',
+      'ỹ': 'y', 'đ': 'd',
+    });
+  });
+  it('should strip … symbols', () => {
+    expect(urlize("foo … bar baz")).toEqual("foo-bar-baz");
+  });
+  it('should strip symbols', () => {
+    const char_map = ['†', '“', '”', '‘', '’', '•'];
+    for (const char of char_map) {
+      expect(urlize(`foo ${char} bar baz`)).toEqual("foo-bar-baz");
+    }
+  });
+  it('should replace lithuanian characters', () => {
+    expect(urlize('ąčęėįšųūžĄČĘĖĮŠŲŪŽ')).toEqual('aceeisuuzaceeisuuz');
+  });
+  it('should default to lowercase in rfc3986 mode', () => {
+    const text = "It's Your Journey We Guide You Through.";
+    const expected = "its-your-journey-we-guide-you-through.";
+    expect(
+      urlize(text,
+      {
+        mode: 'rfc3986'
+      })
+    ).toEqual(expected);
+  });
+});
